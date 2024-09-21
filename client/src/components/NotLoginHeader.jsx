023066cc9e8d0b6bbfa50661d66ad5a469fd7028
ayhan219 <ayhan-games@hotmail.com>
2024-09-21 22:56:08 +0300

    notlogin changed

--- client/src/components/NotLoginHeader.jsx
+++ client/src/components/NotLoginHeader.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'; // Ensure you have react-router-dom ins
 const NotLoginHeader = () => {
   return (
     <div className='w-full h-screen bg-[url("https://plus.unsplash.com/premium_photo-1676057060928-c717a8e96784?q=80&w=1916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-no-repeat flex flex-col items-center justify-center text-white'>
-      <div className='text-4xl mb-4 font-bold'>
+      <div className='text-4xl mb-4 font-bold text-center'>
         If you want to chat, please log in or sign up
       </div>
       <div className='flex space-x-4'>
